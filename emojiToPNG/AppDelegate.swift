dc1bcd31ba36db9354308a379a303939a4bbd50e
Danny182 <danny.zanchi@gmail.com>
2016-08-09 11:29:02 +0200

    First commit

---
+++ emojiToPNG/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  emojiToPNG
+//
+//  Created by Daniel Zanchi on 07/08/16.
+//  Copyright © 2016 Daniel Zanchi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
